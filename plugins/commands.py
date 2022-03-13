e48a6c94607f6d05fa9bd552094ae74bb7d5fea0
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-03-13 16:47:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -273,21 +273,7 @@ async def bot(bot, message):
     await asyncio.sleep(50)
     await h.delete()
        
-@Client.on_message(filters.command("update"))
-async def bot(bot, message):
-    buttons = [[
-        InlineKeyboardButton("ᴅɪᴅɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇ", url='t.me/moviesupdateck')       
-        ],[
-        InlineKeyboardButton("ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅꜱ", url='t.me/cinemakodathi')
-    ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    h=await message.reply_photo(
-        photo=random.choice(PICS),
-        caption=script.UPDATE_CMD.format(message.from_user.mention),
-        reply_markup=reply_markup,
-        parse_mode="html")
-    await asyncio.sleep(30)
-    await h.delete()
+
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
