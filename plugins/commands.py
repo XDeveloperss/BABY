e35b78fe10b2c61142f01679df4cf165e35b0019
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-03-14 08:13:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -242,19 +242,6 @@ async def start(client, message):
         caption=f_caption,
         protect_content=True if pre == 'filep' else False,
         )
-@Client.on_message(filters.regex("hello"))
-async def regex(bot, msg):
-    buttons = [[
-        InlineKeyboardButton("ᴅɪᴅɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇ", url='t.me/moviesupdateck')       
-        ],[
-        InlineKeyboardButton("ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅꜱ", url='t.me/cinemakodathi')
-    ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await msg.reply_photo(
-        photo="https://telegra.ph/file/028b840597616b48ac356.jpg",
-        caption=script.UPDATE_CMD.format(msg.from_user.mention),
-        reply_markup=reply_markup,
-        parse_mode="html")
 
 @Client.on_message(filters.command("moviekittan"))
 async def moviekittan(bot, message):
