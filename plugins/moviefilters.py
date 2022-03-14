9a7f7ec990fc299caad80afba611466271a3ffd3
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-03-14 08:14:01 +0530

    Create moviefilters.py

---
+++ plugins/moviefilters.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+
+@Client.on_message(filters.regex("hi"))
+async def regex(bot, msg):
+    buttons = [[
+        InlineKeyboardButton("ᴅɪᴅɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇ", url='t.me/moviesupdateck')       
+        ],[
+        InlineKeyboardButton("ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅꜱ", url='t.me/cinemakodathi')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await msg.reply_photo(
+        photo="https://telegra.ph/file/028b840597616b48ac356.jpg",
+        caption=script.UPDATE_CMD.format(msg.from_user.mention),
+        reply_markup=reply_markup,
+        parse_mode="html")
