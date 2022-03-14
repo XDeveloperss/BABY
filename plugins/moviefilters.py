223a44c3d6f62087aa977ac1abfda39fa35b14ce
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-03-14 17:11:57 +0530

    Update moviefilters.py

--- plugins/moviefilters.py
+++ plugins/moviefilters.py
@@ -5,9 +5,21 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 @Client.on_message(filters.regex("hi"))
 async def regex(bot, msg):
     buttons = [[
-        InlineKeyboardButton("Fast and furious", callback_data="ftfuri")      
+        InlineKeyboardButton("Fast and furious", callback_data="ftfuri")
+        ],[      
+        InlineKeyboardButton("2001", callback_data="ftfuri"),
+        InlineKeyboardButton("2003", callback_data="ftfuri"),
+        InlineKeyboardButton("2006", callback_data="ftfuri"),
+        InlineKeyboardButton("2009", callback_data="ftfuri")
         ],[
-        InlineKeyboardButton("ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅꜱ", url='t.me/cinemakodathi')
+        InlineKeyboardButton("2011", callback_data="ftfuri"),
+        InlineKeyboardButton("2013", callback_data="ftfuri"),
+        InlineKeyboardButton("2015", callback_data="ftfuri"),
+        InlineKeyboardButton("2017", callback_data="ftfuri")
+        ],[
+        InlineKeyboardButton("Fast And Furious Presents: Hobbs and Shaw", callback_data="ftfuri")
+        ],[
+        InlineKeyboardButton("F9 The Fast Saga", callback_data="ftfuri")
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await msg.reply_photo(
