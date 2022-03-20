2c517fbdc2d95d4b3ab4fc8fd2ac7ada8fe3d9da
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-03-20 10:40:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -251,6 +251,7 @@ async def moviekittan(bot, message):
         InlineKeyboardButton("ᴄɪɴᴇᴍᴀ ᴋᴏᴅᴀᴛʜɪ", url='t.me/cinemakodathi')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
+    await message.action(typing)
     h=await message.reply_photo(
         photo=random.choice(PICS),
         caption=script.MOVIE_KITTAN.format(message.from_user.mention),
@@ -271,6 +272,7 @@ async def bot(bot, message):
         InlineKeyboardButton("ᴊᴏɪɴ ᴍy ɢʀᴏᴜᴩ", url='t.me/cinemakodathi')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
+    await message.action(typing)
     h=await message.reply_photo(
         photo=random.choice(PICS),
         caption=script.BOT_TXT.format(message.from_user.mention),
