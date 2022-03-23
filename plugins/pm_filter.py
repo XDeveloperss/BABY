34f7071fa8776393819a9c3233e7ef39505ef5b6
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-03-23 17:19:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -9,7 +9,7 @@ import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
 from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB, \
-    SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE, CH_FILTER
+    SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
@@ -369,7 +369,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 return
             else:
                 await client.send_cached_media(
-                    chat_id=CH_FILTER,
+                    chat_id=AUTH_CHANNEL,
                     file_id=file_id,
                     caption=f_caption,
                     protect_content=True if ident == "filep" else False 
@@ -425,7 +425,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             f_caption = f"{title}"
         await query.answer()
         await client.send_cached_media(
-            chat_id=CH_FILTER,
+            chat_id=AUTH_CHANNEL,
             file_id=file_id,
             caption=f_caption,
             protect_content=True if ident == 'checksubp' else False
