822ee13000929fdf44f97a8416fcfca4c9c7d085
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-04-12 11:24:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/farshadpyt/BABY-KUTTY.git /BABY-KUTTY
+  git clone https://github.com/farshadpyt/BABY /BABY
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /BABY-KUTTY
+  git clone $UPSTREAM_REPO /BABY
 fi
-cd /BABY-KUTTY
+cd /BABY
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
