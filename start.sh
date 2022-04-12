532be99fc913611519246cca03cf886a11519b16
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2022-04-12 11:16:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /BABY-KUTTY
 fi
-cd /evarandamattebot
+cd /BABY-KUTTY
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
